5a03bae767fd7e9cb131409808dc1812c3934cf1
Jose Carlos Esmeraldo <zeca.esmeraldo@gmail.com>
2019-05-06 16:38:53 -0300

    Teste DB

---
+++ db/migrate/20190425184306_add_index_to_usuarios_email.rb
@@ -0,0 +1,4 @@
+class AddIndexToUsuariosEmail < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_185004) do
+ActiveRecord::Schema.define(version: 2019_04_25_184306) do
 
   create_table "pets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nome"
