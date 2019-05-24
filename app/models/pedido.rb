8c5fc8d8efcfef90849377b6fdcc1a6faeaa3a00
Jose Carlos Esmeraldo <zeca.esmeraldo@gmail.com>
2019-05-24 20:12:47 -0300

    Adicionando email no retorno dos pedidos por usuario

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -3,9 +3,18 @@ class Pedido < ApplicationRecord
   belongs_to :usuario
   attribute :nome_usuario, :string
   attribute :nome_pet, :string
+  attribute :email_usuario, :string
 
   def nome_usuario
-    usuario.nome
+    pet.usuario.nome
+  end
+
+  def email_usuario
+    pet.usuario.email
+  end
+
+  def nome_pet
+    pet.nome
   end
 
   def nome_pet
